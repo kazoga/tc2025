f033f2d3557ebf344d7013b753ffeba74c92957e
kazoga <47599911+kazoga@users.noreply.github.com>
2025-10-30 22:40:58 +0900

    Include all parameter files in package installation

--- ros2_src/obstacle_monitor/setup.py
+++ ros2_src/obstacle_monitor/setup.py
@@ -2,20 +2,40 @@ import os
 from glob import glob
 
 from setuptools import setup
+from typing import List, Tuple
 
 package_name = 'obstacle_monitor'
+
+def list_data_files(target_dir: str) -> List[Tuple[str, List[str]]]:
+    entries = []
+    for root, _, files in os.walk(target_dir):
+        if not files:
+            continue
+        install_dir = os.path.join('share', package_name, os.path.relpath(root, '.'))
+        src_files = [os.path.join(root, file_name) for file_name in files]
+        entries.append((install_dir, src_files))
+    return entries
+
+data_files = [
+    ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+    ('share/' + package_name, ['package.xml']),
+]
+
+map_files = glob('map/*')
+if map_files:
+    data_files.append((os.path.join('share', package_name, 'map'), map_files))
+
+if os.path.isdir('launch'):
+    data_files.extend(list_data_files('launch'))
+
+if os.path.isdir('params'):
+    data_files.extend(list_data_files('params'))
 
 setup(
     name=package_name,
     version='0.2.0',
     packages=[package_name],
-    data_files=[
-        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
-        (os.path.join('share', package_name, 'params'), glob('params/*.yaml')),
-        (os.path.join('share', package_name, 'map'), glob('map/*')),
-    ],
+    data_files=data_files,
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='auto',
--- ros2_src/route_manager/setup.py
+++ ros2_src/route_manager/setup.py
@@ -1,18 +1,34 @@
-from glob import glob
+import os
+
 from setuptools import setup
+from typing import List, Tuple
 
 package_name = 'route_manager'
+
+def list_data_files(target_dir: str) -> List[Tuple[str, List[str]]]:
+    entries = []
+    for root, _, files in os.walk(target_dir):
+        if not files:
+            continue
+        install_dir = os.path.join('share', package_name, os.path.relpath(root, '.'))
+        src_files = [os.path.join(root, file_name) for file_name in files]
+        entries.append((install_dir, src_files))
+    return entries
+
+data_files = [
+    ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+    ('share/' + package_name + '/launch', ['launch/route_manager.launch.py']),
+    ('share/' + package_name, ['package.xml']),
+]
+
+if os.path.isdir('params'):
+    data_files.extend(list_data_files('params'))
 
 setup(
     name=package_name,
     version='0.1.0',
     packages=[package_name],
-    data_files=[
-        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
-        ('share/' + package_name + '/launch', ['launch/route_manager.launch.py']),
-        ('share/' + package_name + '/params', glob('params/*.yaml')),
-        ('share/' + package_name, ['package.xml']),
-    ],
+    data_files=data_files,
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='kazuki',
